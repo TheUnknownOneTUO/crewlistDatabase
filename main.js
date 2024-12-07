65f367991b87d8707780348bc2522fd39b6a1782
TheUnknownOneTUO <157369993+TheUnknownOneTUO@users.noreply.github.com>
2024-12-07 14:53:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+////////////////////////////////////////
+//
+//[SECTION] FOR UTILITY FUNCTIONS
+//
+////////////////////////////////////////
+
 function show(id) {
 
   const element = document.getElementById(id)
@@ -11,6 +17,103 @@ function hide(id) {
   element.classList.add('isHidden')
 
 }
+
+////////////////////////////////////////
+//
+//[SECTION] FOR USER INTERFACE
+//
+////////////////////////////////////////
+
+let pageInView = 'home'
+
+const homeNavBtn = document.getElementById('homeNavBtn')
+const databaseNavBtn = document.getElementById('databaseNavBtn')
+const crewlistNavBtn = document.getElementById('crewlistNavBtn')
+const othersNavBtn = document.getElementById('othersNavBtn')
+
+homeNavBtn.addEventListener('click', function() {
+
+  pageInView = 'home'
+  navBarVisualSelector()
+
+  show('inputHomeDiv')
+  hide('fullDatabaseParentDiv')
+  hide('crewlistGeneratorParentDiv')
+  hide('otherFunctionsDiv')
+
+})
+
+databaseNavBtn.addEventListener('click', function() {
+
+  pageInView = 'database'
+  navBarVisualSelector()
+
+  hide('inputHomeDiv')
+  show('fullDatabaseParentDiv')
+  hide('crewlistGeneratorParentDiv')
+  hide('otherFunctionsDiv')
+
+})
+
+crewlistNavBtn.addEventListener('click', function() {
+
+  pageInView = 'crewlist'
+  navBarVisualSelector()
+
+  hide('inputHomeDiv')
+  hide('fullDatabaseParentDiv')
+  show('crewlistGeneratorParentDiv')
+  hide('otherFunctionsDiv')
+
+})
+
+othersNavBtn.addEventListener('click', function() {
+
+  pageInView = 'others'
+  navBarVisualSelector()
+
+  hide('inputHomeDiv')
+  hide('fullDatabaseParentDiv')
+  hide('crewlistGeneratorParentDiv')
+  show('otherFunctionsDiv')
+
+})
+
+function navBarVisualSelector() {
+
+  if (pageInView === 'home') {
+
+    homeNavBtn.style.background = 'gainsboro'
+    databaseNavBtn.style.background = ''
+    crewlistNavBtn.style.background = ''
+    othersNavBtn.style.background = ''
+
+  } else if (pageInView === 'database') {
+
+    homeNavBtn.style.background = ''
+    databaseNavBtn.style.background = 'gainsboro'
+    crewlistNavBtn.style.background = ''
+    othersNavBtn.style.background = ''
+
+  } else if (pageInView === 'crewlist') {
+
+    homeNavBtn.style.background = ''
+    databaseNavBtn.style.background = ''
+    crewlistNavBtn.style.background = 'gainsboro'
+    othersNavBtn.style.background = ''
+
+  } else if (pageInView === 'others') {
+
+    homeNavBtn.style.background = ''
+    databaseNavBtn.style.background = ''
+    crewlistNavBtn.style.background = ''
+    othersNavBtn.style.background = 'gainsboro'
+
+  }
+
+}
+
+navBarVisualSelector()
 
 ////////////////////////////////////////
 //
@@ -40,7 +143,6 @@ inputPreviewBtn.addEventListener('click', function() {
 
   //acquire the values of input
   acquireInputValues()
-  console.log('values acquired')
 
   //check if the date formatting is consistent
   //* for embarkation dates
@@ -52,7 +154,6 @@ inputPreviewBtn.addEventListener('click', function() {
 
   }
 
-  console.log('dates are consistent')
 
   //* for disembarkation dates
   const isDisembarkationDateFormatConsistent = checkForConsistentDateFormatting(disembarkationArray)
@@ -63,20 +164,16 @@ inputPreviewBtn.addEventListener('click', function() {
 
   }
 
-  console.log('dates are consistent')
 
   //convert the embarakation and disembarkation dates to appropriate format
 
   formatDate(embarkationArray, 'embarkation')
   formatDate(disembarkationArray, 'disembarkation')
 
-  console.log('dates are formatted')
 
   //convert embarkation and disembarkation dates to date objects and push into their respective arrays
 
   toUnixEpoch()
-
-  console.log('dates are converted')
 
   //check and compare if every arrays have the same length
   const isSameLength = checkArrayLengths()
@@ -87,20 +184,15 @@ inputPreviewBtn.addEventListener('click', function() {
 
   };
 
-  console.log('all arrays are same length')
 
   combineAllInfo()
-
-  console.log('all info combined')
 
   //generate the content for html table for previewing
 
   generateInputPreviewContent()
 
-  console.log('content generated')
-
   //Lastly, show the table
-
+  show('inputPreviewParentDiv')
   show('inputPreviewDiv')
 
 })
@@ -214,7 +306,7 @@ function resetBatchInputArrays() {
 
   chronEmbarkationArray = []
   chronDisembarkationArray = []
-  
+
   readableEmbarkationArray = []
   readableDisembarkationArray = []
 
@@ -403,7 +495,7 @@ function formatDate(dataArray, arrayType) {
 
         const monthNumber = i + 1
         month = monthNumber.toString()
-        
+
 
       }
 
@@ -426,15 +518,15 @@ function formatDate(dataArray, arrayType) {
 
     //combine and replace
     if (arrayType === 'embarkation') {
-      
+
       readableEmbarkationArray.push(dataArray[i])
-      
+
     } else if (arrayType === 'disembarkation') {
-      
+
       readableDisembarkationArray.push(dataArray[i])
-      
+
     }
-    
+
     //replace the dates
     dataArray[i] = `${year}-${month}-${day}`
 
@@ -494,7 +586,7 @@ function combineAllInfo() {
 
     slot.chronEmbarkation = chronEmbarkationArray[i]
     slot.chronDisembarkation = chronDisembarkationArray[i]
-    
+
     slot.readableEmbarkation = readableEmbarkationArray[i]
     slot.readableDisembarkation = readableDisembarkationArray[i]
 
@@ -544,8 +636,10 @@ const databaseDupTableBody = document.getElementById('databaseDupTableBody')
 pushToDatabaseBtn.addEventListener('click', function() {
 
   checkForInputDuplicates()
+  hide('inputPreviewParentDiv')
   hide('inputPreviewDiv')
 
+  show('duplicateReviewParentDiv')
   show('duplicateReviewDiv')
 
   if (foundInputDuplicatesArray.length === 0) {
@@ -557,11 +651,11 @@ pushToDatabaseBtn.addEventListener('click', function() {
 
     hide('noInputDupText')
     show('inputDupTable')
-    
+
     generateInputDupTable()
 
   }
-  
+
   ///////////)
 
   if (foundDatabaseDuplicatesArray.length === 0) {
@@ -574,12 +668,14 @@ pushToDatabaseBtn.addEventListener('click', function() {
     hide('noDatabaseDupText')
     show('databaseDupTable')
 
+    generateDatabaseDupTable()
+
   }
-  
+
   if (foundInputDuplicatesArray.length === 0 && foundDatabaseDuplicatesArray.length === 0) {
-  
+
     show('closeDupReviewBtn')
-  
+
   }
 
 })
@@ -587,9 +683,11 @@ pushToDatabaseBtn.addEventListener('click', function() {
 const closeDupReviewBtn = document.getElementById('closeDupReviewBtn')
 
 closeDupReviewBtn.addEventListener('click', function() {
-  
+
+  show('duplicateReviewParentDiv')
   hide('duplicateReviewDiv')
-  
+  hide('duplicateReviewParentDiv')
+
 })
 
 let MAINDATABASEARRAY = []
@@ -684,27 +782,27 @@ function checkForDatabaseDuplicates(passedArray) {
 
 
   })
-  
+
   pushValidEntriesToDatabase()
 
 }
 
 function pushValidEntriesToDatabase() {
-  
+
   nonDuplicatesArray.forEach((obj, i) => {
-    
+
     MAINDATABASEARRAY.push(obj)
-    
+
   })
-  
+
   nonDuplicatesArray = []
-  
+
 }
 
 function generateInputDupTable() {
-  
+
   inputDupTableBody.innerHTML = ''
-  
+
   if (foundInputDuplicatesArray.length === 0) {
 
     show('noInputDupText')
@@ -718,30 +816,30 @@ function generateInputDupTable() {
     show('inputDupHead')
 
   }
-  
+
   if (foundDatabaseDuplicatesArray.length === 0) {
-  
+
     show('noDatabaseDupText')
     hide('databaseDupTable')
     hide('databaseDupHead')
-  
+
   } else if (foundDatabaseDuplicatesArray.length > 0) {
-  
+
     hide('noDatabaseDupText')
     show('databaseDupTable')
     show('databaseDupHead')
-  
+
   }
-  
+
   if (foundInputDuplicatesArray.length === 0 && foundDatabaseDuplicatesArray.length === 0) {
-    
+
     show('closeDupReviewBtn')
-    
+
   }
-    
-    foundInputDuplicatesArray.forEach((obj, i) => {
-      
-      inputDupTableBody.innerHTML += `
+
+  foundInputDuplicatesArray.forEach((obj, i) => {
+
+    inputDupTableBody.innerHTML += `
       
       <tr>
         <td>${obj.familyName}</td>
@@ -755,49 +853,49 @@ function generateInputDupTable() {
       </tr>
       
       `
-      
-    })
-  
+
+  })
+
 }
 
 function manualAddInputDup(i) {
-  
+
   let toTestForDatabase = []
-  
+
   toTestForDatabase.push(foundInputDuplicatesArray[i])
-  
+
   //test for dup im database
   checkForDatabaseDuplicates(toTestForDatabase)
   //re-generate database dup table
   generateDatabaseDupTable()
-  
+
   //then splice 
   foundInputDuplicatesArray.splice(i, 1)
-  
+
   generateInputDupTable()
-  
+
 }
 
 function generateDatabaseDupTable() {
-  
+
   databaseDupTableBody.innerHTML = ''
-  
+
   if (foundInputDuplicatesArray.length === 0) {
-  
+
     show('noInputDupText')
     hide('inputDupTable')
     hide('inputDupHead')
-  
+
   } else if (foundInputDuplicatesArray.length > 0) {
-  
+
     hide('noInputDupText')
     show('inputDupTable')
     show('inputDupHead')
-  
+
     generateInputDupTable()
-  
+
   }
-  
+
   if (foundDatabaseDuplicatesArray.length === 0) {
 
     show('noDatabaseDupText')
@@ -811,16 +909,16 @@ function generateDatabaseDupTable() {
     show('databaseDupHead')
 
   }
-  
+
   if (foundInputDuplicatesArray.length === 0 && foundDatabaseDuplicatesArray.length === 0) {
-    
+
     show('closeDupReviewBtn')
-    
+
   }
-    
-    foundDatabaseDuplicatesArray.forEach((obj, i) => {
-      
-      databaseDupTableBody.innerHTML += `
+
+  foundDatabaseDuplicatesArray.forEach((obj, i) => {
+
+    databaseDupTableBody.innerHTML += `
       
       <tr>
         <td>${obj.familyName}</td>
@@ -834,20 +932,20 @@ function generateDatabaseDupTable() {
       </tr>
       
       `
-      
-    })
-  
+
+  })
+
 }
 
 function manualAddDatabaseDup(i) {
-  
+
   MAINDATABASEARRAY.push(foundDatabaseDuplicatesArray[i])
-  
+
   //then splice 
   foundDatabaseDuplicatesArray.splice(i, 1)
   //re-generate database dup table
   generateDatabaseDupTable()
-  
+
 }
 
 ////////////////////////////////////////
@@ -865,37 +963,37 @@ const closeFullDatabaseBtn = document.getElementById('closeFullDatabaseBtn')
 const downloadDatabaseBtn = document.getElementById('downloadDatabaseBtn')
 
 fullDatabaseBtn.addEventListener('click', function() {
-  
+
   generateFullDatabase()
   show('fullDatabaseTable')
   hide('fullDatabaseBtn')
   show('closeFullDatabaseBtn')
   show('downloadDatabaseBtn')
-  
+
 })
 
 closeFullDatabaseBtn.addEventListener('click', function() {
-  
+
   hide('fullDatabaseTable')
   show('fullDatabaseBtn')
   hide('closeFullDatabaseBtn')
   hide('downloadDatabaseBtn')
-  
-  
+
+
 })
 
 downloadDatabaseBtn.addEventListener('click', function() {
-  
+
   saveJson()
-  
+
 })
 
 function generateFullDatabase() {
-  
+
   const tbody = fullDatabaseTableBody
-  
+
   tbody.innerHTML = ''
-  
+
   MAINDATABASEARRAY.forEach((obj, i) => {
 
     tbody.innerHTML += `
@@ -916,7 +1014,7 @@ function generateFullDatabase() {
     
     `
   })
-  
+
 }
 
 
@@ -951,10 +1049,10 @@ const databaseUploadInput = document.getElementById('databaseUploadInput')
 
 const loadFileBtn = document.getElementById('loadFileBtn')
 
-loadFileBtn.addEventListener('click', function () {
-  
+loadFileBtn.addEventListener('click', function() {
+
   loadJson()
-  
+
 })
 
 function loadJson() {
@@ -992,11 +1090,13 @@ const generatedCrewlistTableBody = document.getElementById('generatedCrewlistTab
 const crewlistGeneratorBtn = document.getElementById('crewlistGeneratorBtn')
 
 crewlistGeneratorBtn.addEventListener('click', function() {
-  
+
   filterBasedOnDate()
+  organizeEntriesByEmbarkDate()
   generateCrewlist()
   show('generatedCrewlistTable')
-  
+  show('saveTxtBtn')
+
 })
 
 let crewlistToBeGeneratedArray = []
@@ -1007,25 +1107,190 @@ function filterBasedOnDate() {
 
   MAINDATABASEARRAY.forEach((obj, i) => {
 
-    
+
     const crewListDate = new Date(crewlistDatePickerInput.value)
 
     if (obj.chronEmbarkation <= crewListDate && obj.chronDisembarkation >= crewListDate) {
-
+      
       crewlistToBeGeneratedArray.push(obj)
 
-    }
+    };
+    
 
 
   })
 
 }
 
+//organize the ordering of entries
+
+let organizedByDateArray = []
+let uncategorizedPeople = []
+
+function organizeEntriesByEmbarkDate() {
+
+  let toBeTestedFurtherArray = []
+  uncategorizedPeople = []
+
+  //call the first iteration 
+  determineOldest()
+
+  function determineOldest() {
+
+    //determine the current oldest
+    let oldestDate = null;
+
+
+    crewlistToBeGeneratedArray.forEach((obj, i) => {
+
+      if (oldestDate === null) {
+
+        oldestDate = obj.chronEmbarkation
+
+      } else if (oldestDate !== null && obj.chronEmbarkation < oldestDate) {
+
+        oldestDate = obj.chronEmbarkation
+
+      }
+
+    })
+
+    //another loop to push the ones with similar dates to the oldestDate
+    
+    crewlistToBeGeneratedArray.forEach((obj, i) => {
+      
+      const comparandum = obj.chronEmbarkation.toLocaleDateString()
+      const referent = oldestDate.toLocaleDateString()
+      
+      if (obj.chronEmbarkation > oldestDate) {
+        //to save the remaining entries temporarily 
+        toBeTestedFurtherArray.push(obj)
+
+      } else if (comparandum === referent) {
+        
+        organizedByDateArray.push(obj)
+
+      } else {
+        
+        uncategorizedPeople.push(obj)
+        
+      }
+
+    });
+
+    //delete the current crewlistToBeGenerated
+
+    crewlistToBeGeneratedArray = []
+
+    //push all elements in toBeTestedFurtherArray to crewlistToBeGeneratedArray
+
+    toBeTestedFurtherArray.forEach((obj, i) => {
+      
+      crewlistToBeGeneratedArray.push(obj)
+
+    })
+
+    //reset the toBeTestedFurtherArray
+
+    toBeTestedFurtherArray = []
+
+    //call the function again if there are still left in crewlistToBeGeneratedArray
+
+    if (crewlistToBeGeneratedArray.length > 0) {
+
+      determineOldest()
+
+    }
+
+  }
+  
+  if (uncategorizedPeople.length > 0) {
+    
+    const body = document.body 
+    
+    body.style.background = 'red'
+    
+  }
+
+}
+
+//organize by rankings
+
+let organizedByRankArray = []
+
+function organizeEntriesByRank() {
+
+  //loop for MASTER
+
+  organizedByDateArray.forEach((obj, i) => {
+
+    if (obj.rank === 'MASTER') {
+
+      organizedByRankArray.push(obj)
+
+    }
+
+  })
+
+  //loop for CHIEF OFFICER
+
+  organizedByDateArray.forEach((obj, i) => {
+
+    if (obj.rank === 'CHIEF OFFICER') {
+
+      organizedByRankArray.push(obj)
+
+    }
+
+  })
+
+  //loop for DECK OFFICER 1
+
+  organizedByDateArray.forEach((obj, i) => {
+
+    if (obj.rank === 'DECK OFFICER 1') {
+
+      organizedByRankArray.push(obj)
+
+    }
+
+  })
+
+  //loop for DECK OFFICER 2
+
+  organizedByDateArray.forEach((obj, i) => {
+
+    if (obj.rank === 'DECK OFFICER 2') {
+
+      organizedByRankArray.push(obj)
+
+    }
+
+  })
+
+  //loop for DECK RATING 1
+
+  organizedByDateArray.forEach((obj, i) => {
+
+    if (obj.rank === 'DECK RATING 1') {
+
+      organizedByRankArray.push(obj)
+
+    }
+
+  })
+
+
+}
+
+
+//to generate the crewlist
+
 function generateCrewlist() {
-  
+
   generatedCrewlistTableBody.innerHTML = ''
-  
-  crewlistToBeGeneratedArray.forEach((obj, i) => {
+
+  organizedByDateArray.forEach((obj, i) => {
 
     generatedCrewlistTableBody.innerHTML += `
     
@@ -1050,6 +1315,9 @@ function generateCrewlist() {
     `
   })
   
+  console.log(organizedByDateArray)
+  organizedByDateArray = []
+
 }
 
 //FOR SAVING AS TXT FILE
@@ -1057,17 +1325,17 @@ function generateCrewlist() {
 const saveTxtBtn = document.getElementById('saveTxtBtn')
 
 saveTxtBtn.addEventListener('click', function() {
-  
+
   saveTxt()
-  
+
 })
 
 function saveTxt() {
   let text = ''
 
-  crewlistToBeGeneratedArray.forEach((obj, i) => {
+  organizedByDateArray.forEach((obj, i) => {
 
-    text += `${i + 1}\t${obj.familyName}\t${obj.givenName}\t${obj.rank}\tFILIPINO\t${obj.birthday}\t \t${obj.birthPlace}\t${obj.gender}\t \tSIRB/SRB\t \t${obj.idNum}\tPHILIPPINES\t${obj.idExpiry}\n`
+    text += `${i + 1}\t${obj.familyName}\t${obj.givenName}\t${obj.rank}\tFILIPINO\t${obj.birthday}\t${obj.birthPlace}\t${obj.gender}\tSIRB/SRB\t${obj.idNum}\tPHILIPPINES\t${obj.idExpiry}\n`
 
   })
 
@@ -1080,4 +1348,63 @@ function saveTxt() {
   document.body.appendChild(a);
   a.click();
   URL.revokeObjectURL(url);
+}
+
+////////////////////////////////////////
+//
+//[SECTION] FOR MISC FUNCTIONS
+//
+////////////////////////////////////////
+
+const columnForFormulaInput = document.getElementById('columnForFormulaInput')
+
+const rowStartForFormulaInput = document.getElementById('rowStartForFormulaInput')
+
+const rowEndForFormulaInput = document.getElementById('rowEndForFormulaInput')
+
+const formulaClipboard = document.getElementById('formulaClipboard')
+
+const generateFormulaBtn = document.getElementById('generateFormulaBtn')
+
+generateFormulaBtn.addEventListener('click', function() {
+
+  generateExcelFormula()
+
+})
+
+//to generate the formula
+
+function generateExcelFormula() {
+
+  formulaClipboard.innerHTML = ''
+
+  //acquire the values
+
+  const column = columnForFormulaInput.value
+  const rowStart = rowStartForFormulaInput.value
+  const rowEnd = rowEndForFormulaInput.value
+
+  const start = parseInt(rowStart)
+  const end = parseInt(rowEnd)
+
+  //start of formula:
+
+  formulaClipboard.innerHTML += 'CONCATENATE('
+
+  for (let i = start; i <= end; i++) {
+
+    if (i !== end) {
+
+      formulaClipboard.innerHTML += `'#', ${column}${i},`
+
+    } else if (i === end) {
+
+      formulaClipboard.innerHTML += `'#', ${column}${i}`
+
+    }
+
+  }
+
+  formulaClipboard.innerHTML += ')'
+
 }
